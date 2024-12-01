63618a68fb89433480241025dbf8fce8351fcbec
Vaibhav Maini <ph03n1xc0d3s@gmail.com>
2024-12-01 20:42:57 +0530

    Updated navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import '../assets/css/navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="main-navbar">
+      <div className="name__logo">
+        <h3 className="name">Vaibhav Maini</h3>
+      </div>
+      <div className="link-list">
+        <ul className="ul--list">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
